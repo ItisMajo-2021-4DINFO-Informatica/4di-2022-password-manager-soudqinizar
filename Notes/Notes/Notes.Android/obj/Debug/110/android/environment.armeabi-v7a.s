5de4db2919f91922969c2d1ea1cdc3a122793f12
Soudqi Nizar <soudqinizar@gmail.com>
2022-02-09 21:45:43 +0100

    Immagine cambiata + perfezionamenti

--- Notes/Notes/Notes.Android/obj/Debug/110/android/environment.armeabi-v7a.s
+++ Notes/Notes/Notes.Android/obj/Debug/110/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"522042ce-a95a-4db2-b6c4-8d6fe5e3cdd4"
+	.asciz	"3919fb75-0301-442e-819a-3484b9396b43"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
--- Notes/Notes/Notes/Views/AboutPage.xaml.cs
+++ Notes/Notes/Notes/Views/AboutPage.xaml.cs
@@ -14,7 +14,7 @@ namespace Notes.Views
         async void OnButtonClicked(object sender, EventArgs e)
         {
             // Launch the specified URL in the system browser.
-            await Launcher.OpenAsync("https://aka.ms/xamarin-quickstart");
+            await Launcher.OpenAsync("https://github.com/soudqinizar");
         }
     }
 }
